1327c319f7cac9cd7ca80ebdea04642691740f31
TrandyAndy <43793912+TrandyAndy@users.noreply.github.com>
2020-03-07 21:18:17 +0100

    Motor finaler Schliff

--- lib/Motor/Motor.h
+++ lib/Motor/Motor.h
@@ -1,20 +1,20 @@
 class Motor
 {
-  public:                               // öffentlich
-    Motor();                            // der Default-Konstruktor
-    int Run();                          //in loop!!!! -1=Error Richtungswechsel, 0=Volle Fahrt, 1=Hochfahren
-    void setMotorStart(bool mRichtung); //Startet den Motor. Richtungswechsel nur mit vorigen Motor Stopp möglich! 
-    void setMotorStopp();               //Anhalten
-    int getMotorSpeed();                //int??? gibt die Geschwindigkeit in % zurück.
-    bool getMotoristRichtung();         //sagt einem die aktuelle Drehrichtung
-    void Not_Aus();
-    void setFrequenz(int f);            //ändert die Motorfrequenz zwischen 800-10.000 Hz.
-    void changeSpeed(int speed);        //anpassen der Default Geschwindigkeit.  VORSICHT!
+  public:                                     // öffentlich
+    Motor(int PWM_Pin, int DIR_Pin);          // der Konstruktor
+    int Run();                                //in loop!!!! -1=Error Richtungswechsel, 0=Volle Fahrt, 1=Hochfahren
+    void setMotorStart(bool mRichtung);       //Startet den Motor. Richtungswechsel nur mit vorigen Halt möglich! Halt kann mit getMotorSpeed()==0 ermittelt werden.
+    void setMotorStopp();                     //Anhalten
+    int getMotorSpeed();                      //gibt die Geschwindigkeit in % zurück.
+    bool getMotoristRichtung();               //gibt die aktuelle Drehrichtung
+    void Not_Aus();                           //sofort Abschalten.
+    void setFrequenz(int f);                  //ändert die Motorfrequenz zwischen 800-10.000 Hz.
+    void changeSpeed(int speed);              //anpassen der Default Geschwindigkeit.  VORSICHT!!
     ~Motor();                    
 
     
 
-  private:                              // privat
+  private:                                    // privat
     void AusgangsPower(int Pin, int Power);
     unsigned int istSpeed=0;
     bool Stopp=true;
