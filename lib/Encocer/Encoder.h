25565d13b7e011027ee136a9b4afd1afd30ef335
JLS666 <43670180+JLS666@users.noreply.github.com>
2020-03-08 22:53:44 +0100

    Encoder frisch gemacht

--- lib/Encocer/Encoder.h
+++ lib/Encocer/Encoder.h
@@ -1,13 +1,20 @@
+/********************************************************************/
+/*  Autoren:      Julian Schweizerhof                               */
+/*  Firma:        Hochschule Karlsruhe - Technik u. Wirtschaft      */
+/*  Datei:        Encoder.h                                         */
+/*  Beschreibung: Header der Datei der Klasse Encoder               */
+/*  Version:      0.1                                               */                 
+/********************************************************************/
+
 class Encoder
 {
 private:
-    int zaehler;
+    int zaehler;                // Zähler des Encoders
 public:
-    Encoder();
+    Encoder();                      
     ~Encoder();
-    void resetZaehler();
-    int getZaehler();
-    void inkrementZaehler();
-    void dekrementZaehler();
-    void encoderEvent();
+    void resetZaehler();        // Zähler zurücksetzten, bei Endschalter vorne
+    int getZaehler();           // Zähler als GZ zurückgeben
+    void inkrementZaehler();    // Zähler um eins erhöhen 
+    void dekrementZaehler();    // Zähler um eins erniedrigen
 };
